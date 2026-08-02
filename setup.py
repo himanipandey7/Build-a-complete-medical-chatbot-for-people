5352a2ef4866e22d6221546baf18d2dd1847e88c
HIMANI PANDEY <himanipandey07102004@gmail.com>
2026-08-02 17:47:06 +0530

    Initial commit for medical chatbot

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Himani pandey",
+    author_email="himani pandey",
+    packages=find_packages(),
+    install_requires=[]
+)
---
+++ template.sh
@@ -0,0 +1,12 @@
+mkdir -p src
+mkdir -p research
+touch src/__init__.py
+touch src/helper.py
+touch src/Prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/Trials.ipynb
+touch requirements.txt
+echo "Directory and file are created successfully"
+
